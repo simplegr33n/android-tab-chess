51bd114dfd232abcbb0974a7312c1b7e9339b140
simplegr33n <gcgolda@gmail.com>
2016-12-22 14:10:48 -0800

    changed launch to LobbyActivity

--- app/src/main/java/ca/ggolda/android_tab_chess/LobbyActivity.java
+++ app/src/main/java/ca/ggolda/android_tab_chess/LobbyActivity.java
@@ -1,8 +1,18 @@
 package ca.ggolda.android_tab_chess;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by gcgol on 12/22/2016.
  */
 
-public class LobbyActivity {
+public class LobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby);
+
+    }
 }
